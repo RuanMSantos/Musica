8d843a12929c3071bc0d9192b233c4d48e027145
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-18 20:34:23 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.Write("\n");
+Console.Write("Jogador Número 1");
+Console.Write("\n");
+
+Console.ResetColor();
+
+Console.Write("\n");
+
+Console.Write(@"
+Mano, eu já passei dessa fase 
+De tentar e só ficar no quase
+Tô em órbita, piloto a nave
+Vou zerar a vida no level hard
+Eu me sinto o Percy Jackson
+Navegando num Mar de Monstros
+Dizem que eu reencarnei o Michael Jackson
+Se eu sou um rei, então me dá meu trono
+Os deuses colocaram um holofote em mim
+É o meu momento de brilhar
+Eles podem duvidar
+Mas...");
+
+Console.WriteLine("\n");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Quando eles veem o que eu posso fazer
+Acham que eu tenho um superpoder
+Dizem que eu sou incomum
+Eles me chamam de jogador número um
+De cima do palco, eu faço o mundo tremer
+E hoje eu vou cantar até o céu descer
+Highest in the room
+Eles me chamam de jogador número 1 (1, 1)
+Eles me chamam de jogador número 1 (1, 1)
+Eles me chamam de jogador número 1");
+
+Console.ResetColor();
+
+Console.WriteLine("\n");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
+(Sid, rrrr)");
+
+Console.ResetColor();
+
+Console.WriteLine(@"
+Como Uzumaki, eu nunca deixei de ter fé
+Como Uchiha, ainda guardo raiva dos outros
+Eu não nasci abençoado, me chame de Rock Lee
+Foi na base do esforço que eu vim pro topo
+Tô cozinhando hit, estúdio é minha cozinha
+Eu não aceito preconceito e nem tua piadinha
+Meu verso pisoteia verme, me chame de Júlio Verne
+Dei a volta no mundo em 80 linhas
+Já morri mais vezes do que me orgulho
+Eu sou o fruto do silêncio e a causa do barulho
+Eu sou maior que tudo isso, eu juro
+Eu sou o ego que eu machuco e também a alma que eu curo
+Meio psicopata, de um lado Zapata, do outro pirata
+Meu verso tá matando sede que nem rum
+Canto toda a verdade, meu verso é realidade
+Ganhei destaque porque sinceridade virou incomum
+Não tem pose pra foto, não é sorte, é só foco
+O meu brilho desfoca o olho gordo do teu zoom
+Eu não brinco no jogo e nem minto no verso, truta
+Por isso me chamam de jogador número 1");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("1, 1, 1 (ei)");
+Console.WriteLine("Eles me chamam de jogador número 1 ");
+
+Console.WriteLine("\n");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("(Rod)");
+
+Console.ResetColor();
+
+Console.WriteLine("\n");
+
+Console.WriteLine(@"
+Me dá meu trono que eu sou o dono
+Mandalorian, filho do abandono
+E qualquer missão dada, é missão cumprida
+Mesmo que eu tenha que sair com umas feridas
+Sabe que, foram vários quase aqui
+Foram várias traves sim, mas ainda tô aqui
+Zerei o jogo sem nem precisar de treta
+Eu escolhi o nível hard e vim parar nesse planeta
+Você nem ouve rap e quer opinar nas minhas letras
+É igual voltar no tempo sem o efeito borboleta
+Eu sei que é incomum, eu ainda ser o número 1
+Já zerei o jogo tantos anos, esse é só mais um
+Então deixa eu te falar onde eu vou tá no final de 2, 0, 2, 1
+Doidão com os pirata bebendo rum
+No estilo de Jack Sparrow, mano, nada espero
+Dessas pessoas que brotaram só depois do meu sucesso
+Poucos no caminho torcendo pra isso dar certo
+É um blefe, apertam tua mão, depois te flecham (ha, ha)
+Velozes e Furiosos, sabe
+Deixam tu correr depois te fecham
+Sabe que vencer é só um detalhe aqui
+Eu tô no nível hard, sabe que eu brotei de nave aqui
+Esse é o tipo de jogo que eles nunca vão querer que tu controle
+Então liguei o foda-se e fiz meu próprio console (Rod)");
+
+Console.WriteLine("\n");
+
+Console.WriteLine(@"
+Eu sinto suas dúvidas, eu ouço suas desculpas
+Disse que seu fracasso não é culpa sua e sim do mundo de hoje
+É assim mesmo
+Quem não sabe jogar, sempre põe a culpa no controle
+Confia em mim, tá tudo sob controle
+Mark Zuckerberg nos escuta, pôs escutas nos nossos telefones
+E o governo observa nossas crianças
+Pela mira de um drone
+Só quando a gente entende que a coragem nunca dorme
+O medo some
+Andar na corda bamba sem cair, fazer seu nome
+É isso que separa os meninos dos homens
+Me sinto Spider Silva contra Chael Sonnen
+Esse é o anime da minha vida, eu sou protagonista shonen
+Tô com foco na subida, eu sei que o topo é longe
+De vitória e de comida, nós sabe o que é ter fome
+Não queria punchline? Então, tome
+Eu só queria andar de Nike ou Tommy
+'Cês não sabem nem metade do que eu vivi nesse mundo
+Mano, eu já passei vontade, por isso eu vou comprar tudo
+E eles ouvem esse som
+E eles não acreditam
+Dizem que é um absurdo
+Não, isso é 7 Minutoz");
+
+Console.WriteLine("\n");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Quando eles veem o que eu posso fazer
+Acham que eu tenho um superpoder
+Dizem que eu sou incomum
+Eles me chamam de jogador número 1
+De cima do palco, eu faço o mundo tremer
+E hoje eu vou cantar até o céu descer
+Highest in the room
+Eles me chamam de jogador número 1 (1, 1)
+Eles me chamam de jogador número 1 (1, 1)
+Eles me chamam de jogador número 1");
+Console.ResetColor();
